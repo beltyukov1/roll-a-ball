d5aff293571bc7a4eab1de92f6edbc149dfcda7d
Alex Beltyukov <beltyukov.1@gmail.com>
2016-05-28 14:21:02 -0500

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start() {
+		// transform.position is position of the camera
+		offset = transform.position - player.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	// Runs after all items have been processed in Update()
+	void LateUpdate() {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rigidBody;
+
+	// Called on first frame script is active
+	// Often very first frame of game
+	void Start() {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+
+	// Called before rendering a frame 	// Most of game code will go here
+	void Update() {
+	
+	}
+	
+	// Called before performing any physics calculations
+	// Physics code will go here
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+		rigidBody.AddForce(movement * speed);
+	}
+}
